dbc4aed0b25de83f6a0d80d5da140fdc7b755574
qroosje <qroosje@minubia.com>
2018-02-27 08:38:53 -0400

    Made xcodePreferences.js compatible with Cordova v7

--- hooks/lib/ios/xcodePreferences.js
+++ hooks/lib/ios/xcodePreferences.js
@@ -135,16 +135,27 @@ function isPbxReferenceAlreadySet(fileReferenceSection, entitlementsRelativeFile
 function loadProjectFile() {
   var platform_ios;
   var projectFile;
-
-  try {
-    // try pre-5.0 cordova structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-  } catch (e) {
-    // let's try cordova 5.0 structure
+//Run on ios code added by quincy on feb 28, 2018    
+try {
     platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
     projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+  } catch(e) {
+    // try cordova 7.0 structure
+    var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+    var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+    var locations = (new iosPlatformApi()).locations;
+    projectFile = projectFileApi.parse(locations);      
   }
+// Comment code that came with fork, gives a plugman ios error when building
+//  try {
+//    // try pre-5.0 cordova structure
+//    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
+//    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+//  } catch (e) {
+//    // let's try cordova 5.0 structure
+//    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
+//    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+//  }
 
   return projectFile;
 }
